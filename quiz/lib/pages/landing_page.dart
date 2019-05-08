15d2aa6f41853b341c026fd069d5e531942a5972
Mahmoud <m.alhumsialrefaai@student.tudelft.nl>
2019-05-08 13:41:51 +0200

    creating landing page

---
+++ quiz/lib/pages/landing_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.greenAccent,
+      );
+  }
+}
